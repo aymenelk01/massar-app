b9087b0f622136fdf626eff09cf41ed13e67ea81
aymenelk01 <aymenelkharchi15@gmail.com>
2026-06-04 17:53:15 +0200

    feat: add script to setup Cognito user with username and password

---
+++ Scripts/setup-cognito-student.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+export AWS_PAGER=""
+
+
+USER_POOL_ID="eu-south-1_3QYn6cnDA"
+REGION="eu-south-1"
+
+echo "enter username"   
+read -r USERNAME
+
+echo "Enter password for user ${USERNAME}:"
+read -s PASSWORD
+
+
+
+aws cognito-idp admin-create-user \
+  --user-pool-id "${USER_POOL_ID}" \
+  --username "${USERNAME}" \
+  --message-action SUPPRESS \
+  --region "${REGION}"
+
+
+aws cognito-idp admin-set-user-password \
+  --user-pool-id "${USER_POOL_ID}" \
+  --username "${USERNAME}" \
+  --password "${PASSWORD}" \
+  --permanent \
+  --region "${REGION}"
